8627d11f23795c7ffd3765ee6083eefe07967c34
tank <tank@certifiedcoderz.com>
2023-12-27 20:49:42 -0500

    good

--- .biz9_config.sh
+++ .biz9_config.sh
@@ -1,10 +1,9 @@
-BIZ9_SERVER_VERSION='5.3.8'
+BIZ9_SERVER_VERSION='5.3.9'
 APP_VERSION='1.0.0'
 PROJECT_ID="19";
 APP_TITLE="BiZ9-Server";
 SERVER_IP="0.0.0.0"
-SERVER_DEPLOY_DIR="/home/admin/www/100/website"
+SERVER_DIR="/home/admin/www/100/website"
 SSH_KEY=${BIZ9_BOX_SSH_266}
-REPO_URL="git@github.com:biz9framework/biz9-server.git"
-MONGO_PORT="27019"
+GIT_REPO="${BIZ9_GIT_REPO}biz9-server.git"
 PROJECT_LIST=(website-1 website-2)
